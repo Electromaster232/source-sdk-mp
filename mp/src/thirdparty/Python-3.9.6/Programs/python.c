a05b1056ff4035da9b9b73d93a4e2dd310e3b489
Electromaster232 <gilbert7573@gmail.com>
2021-08-09 23:21:33 -0400

    py initial

---
+++ mp/src/thirdparty/Python-3.9.6/Programs/python.c
@@ -0,0 +1,23 @@
+/* Minimal main program -- everything is loaded from the library */
+
+#include "Python.h"
+#include <locale.h>
+
+#ifdef MS_WINDOWS
+
+#include <Windows.h>
+
+
+int
+wmain(int argc, wchar_t **argv)
+{
+    return Py_Main(argc, argv); 
+
+}
+#else
+int
+main(int argc, char **argv)
+{
+    return Py_BytesMain(argc, argv);
+}
+#endif
